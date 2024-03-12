ad7d2aae02ddcf5192e1873638fed22d952ade7e
jaro124 <jaro124@gmail.com>
2024-03-12 18:10:10 +0100

    Adding layout, header, footer and dark mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,27 @@
 module.exports = {
   siteMetadata: {
     title: `starter-gatsby-zero`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/w129-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`, `/blog/*`, `/about`],
+      },
+    },
+  ],
 };
---
+++ src/pages/blog.js
@@ -0,0 +1,44 @@
+import * as React from "react"
+import { Link } from "gatsby";
+import Layout from "../components/core/layout"
+
+const BlogPage = () => {
+  return (
+    <Layout pageTitle="Blog Page">
+      <div className="px-4">
+        <div className="max-w-4xl bg-white dark:bg-black rounded-lg mx-auto my-8 p-16">
+          <h1 className="text-2xl font-medium text-black dark:text-gray-400 mb-2">
+            Blog page
+          </h1>
+          <h2 className="font-medium text-sm text-indigo-400 mb-4 uppercase tracking-wide">
+            List posts
+          </h2>
+          <p className="text-primary">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum illo
+            cupiditate molestias atque consequuntur ea quo cumque, odit velit
+            sint similique? Asperiores ratione aperiam tempora, alias corrupti
+            deleniti quaerat molestiae.
+          </p>
+          <p className="text-primary">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum illo
+            cupiditate molestias atque consequuntur ea quo cumque, odit velit
+            sint similique? Asperiores ratione aperiam tempora, alias corrupti
+            deleniti quaerat molestiae.
+          </p>
+          <div className="mt-4">
+            <button
+              aria-label="Back to home page"
+              className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-4 mt-2"
+            >
+              <Link to="/">Back to home page</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default BlogPage
+
+export const Head = () => <title>Blog Page</title>
